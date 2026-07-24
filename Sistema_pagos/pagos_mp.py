dd7915a30e81982d87e577fadfb2fec38c13c7ce
pedrocolumnaalv <pe_droantonio@hotmail.com>
2026-07-24 15:35:30 -0600

    Activar auto_return y FRONTEND_URL para produccion en Vercel

--- Sistema_pagos/pagos_mp.py
+++ Sistema_pagos/pagos_mp.py
@@ -74,8 +74,8 @@ def crear_pago(
     "success": f"{FRONTEND_URL}/pago/exitoso",
     "failure": f"{FRONTEND_URL}/pago/fallido",
     "pending": f"{FRONTEND_URL}/pago/pendiente"
-}
-      #  "auto_return": "approved"
+},
+        "auto_return": "approved"
     }
 
     try:
